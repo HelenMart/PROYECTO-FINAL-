9bc266af9955f1e22d6ac7d44decc15327389d25
ElleMartinez <hmartinezs@miumg.edu.gt>
2026-05-25 21:17:31 -0600

    Modulo 5 backup recovery y nube completado

--- backend/cron/backups.js
+++ backend/cron/backups.js
@@ -4,18 +4,29 @@ const fs = require("fs");
 const iniciarBackups = () => {
 
     cron.schedule("*/2 * * * *", () => {
-        
+
         const fecha = new Date()
             .toISOString()
             .replace(/:/g, "-");
 
-        const ruta = "/app/backups/backup-" + fecha + ".sql";
+        const ruta =
+            "/app/backups/backup-" + fecha + ".sql";
+
+        const rutaCloud =
+            "/app/cloud_backups/cloud-" + fecha + ".sql";
+
         fs.writeFileSync(
             ruta,
             "RESPALDO AUTOMATICO DEL SISTEMA"
         );
 
+        fs.copyFileSync(
+            ruta,
+            rutaCloud
+        );
+
         console.log("BACKUP GENERADO");
+
     });
 };
 
